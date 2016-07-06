87d7b2c7b7fd3dfcb8729499e220b2776be8b7df
Yura Voevodin <voevodin.yura@icloud.com>
2016-07-06 14:02:09 +0300

    Updated macOS deployment target

---
+++ Example/Pods/Target Support Files/CoreDuck-OSX/CoreDuck-OSX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CoreDuck_OSX : NSObject
+@end
+@implementation PodsDummy_CoreDuck_OSX
+@end
---
+++ Example/Pods/Target Support Files/Pods-CoreDuck Example macOS-CoreDuck_Example_macOS/Pods-CoreDuck Example macOS-CoreDuck_Example_macOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_CoreDuck_Example_macOS_CoreDuck_Example_macOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CoreDuck_Example_macOS_CoreDuck_Example_macOSVersionString[];
+
