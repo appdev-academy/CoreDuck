8d3527c2a3e41ffc793cd81b8d69013ac6f3339f
Yura Voevodin <voevodin.yura@icloud.com>
2016-07-06 13:30:23 +0300

    Updated Podfile

---
+++ Example/Pods/Target Support Files/Pods-CoreDuck Example iOS Test-CoreDuck_Tests/Pods-CoreDuck Example iOS Test-CoreDuck_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDuck_Example_iOS_Test_CoreDuck_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDuck_Example_iOS_Test_CoreDuck_Tests
+@end
