bbbd8764b8914a84e620de1129c1216892ff5a03
Yura Voevodin <voevodin.yura@icloud.com>
2016-07-06 13:34:36 +0300

    Added macOS target

---
+++ Example/CoreDuck_Example_macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreDuck_Example_macOS
+//
+//  Created by Yura Voevodin on 06.07.16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/CoreDuck_Example_macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreDuck_Example_macOS
+//
+//  Created by Yura Voevodin on 06.07.16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
