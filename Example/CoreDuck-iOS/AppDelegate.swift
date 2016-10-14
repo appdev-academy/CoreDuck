b639674063411e11f41af7fb7f9b80488ab18ebf
Maksym Skliarov <maksym.skliarov@gmail.com>
2016-10-14 12:23:00 +0300

    Version 1.0.0 for Swift 3.0

--- Example/CoreDuck-iOS/AppDelegate.swift
+++ Example/CoreDuck-iOS/AppDelegate.swift
@@ -11,59 +11,69 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    var window: UIWindow?
+    // Initialize CoreData stack
+    let _ = CoreDuck.quack
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        // Initialize CoreData stack
-        let _ = CoreDuck.quack
-        
-        // Access main NSManagedContext
-        let _ = CoreDuck.quack.mainContext
-        // or
-        let _ = NSManagedObjectContext.mainContext
-        
-        // Get background NSManagedObjectContext
-        let _ = CoreDuck.quack.backgroundContext
-        // or
-        let _ = NSManagedObjectContext.backgroundContext
-        
-        // Make some changes to NSManagedObjects
-        NSManagedObjectContext.saveWithBlock({
-            localContext in
-        }, completion: {
-            success in
-        })
-        // or
-        NSManagedObjectContext.saveWithBlockAndWait({
-            localContext in
-        }, completion: {
-            success in
-        })
-        
-        return true
-    }
+    // Access main NSManagedContext
+    let _ = CoreDuck.quack.mainContext
+    // or
+    let _ = NSManagedObjectContext.mainContext
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
+    // Get background NSManagedObjectContext
+    let _ = CoreDuck.quack.backgroundContext
+    // or
+    let _ = NSManagedObjectContext.backgroundContext
     
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+    // Make some changes to NSManagedObjects
+    NSManagedObjectContext.saveWithBlock({
+      localContext in
+      
+      // Save NSManagedObjects here...
+      
+    }, completion: {
+      success in
+      
+      // Completion code depending on success bool
+    })
+    // or
+    NSManagedObjectContext.saveWithBlockAndWait({
+      localContext in
+      
+      // Save NSManagedObjects here...
+      
+    }, completion: {
+      success in
+      
+      // Completion code depending on success bool
+    })
     
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    return true
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  }
+  
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
 }
--- Example/CoreDuck-macOS/ViewController.swift
+++ Example/CoreDuck-macOS/ViewController.swift
@@ -9,19 +9,16 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override var representedObject: Any? {
+    didSet {
+      // Update the view, if already loaded.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+  }
 }
-
